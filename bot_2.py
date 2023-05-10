a420ae383a7b8607a8a9582d19df09ab24d06853
vladimirsaligin <saliginvladimir@gmail.com>
2023-05-10 21:51:10 +0300

    telegram

--- bot_2.py
+++ bot_2.py
@@ -1,76 +1,38 @@
-# sample-bot.py
-from telegram import InlineQueryResultArticle, InputTextMessageContent
-from telegram.ext import Updater, CommandHandler
-from telegram.ext import MessageHandler, filters, InlineQueryHandler
+import telegram
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-TOKEN = '5987197501:AAFh8an4ZvHgwyFhjw7sQkU6gsAlS3qIjqo'
-updater = Updater(token=TOKEN)
-dispatcher = updater.dispatcher
-
-# функция обработки команды '/start'
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="I'm a bot, please talk to me!")
+    """Обработчик команды /start"""
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Привет, я телеграм-бот! Я могу повторять за вами любые текстовые сообщения, которые вы мне отправите.')
 
-# функция обработки текстовых сообщений
+def help(update, context):
+    """Обработчик команды /help"""
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Это помощь!')
+
 def echo(update, context):
-    text = 'ECHO: ' + update.message.text
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text=text)
+    """Обработчик текстовых сообщений"""
+    text = update.message.text
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
-# функция обработки команды '/caps'
-def caps(update, context):
-    if context.args:
-        text_caps = ' '.join(context.args).upper()
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text=text_caps)
-    else:
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text='No command argument')
-        context.bot.send_message(chat_id=update.effective_chat.id,
-                                 text='send: /caps argument')
+def main():
+    """Основная функция бота"""
 
-# функция обработки встроенного запроса
-def inline_caps(update, context):
-    query = update.inline_query.query
-    if not query:
-        return
-    results = list()
-    results.append(
-        InlineQueryResultArticle(
-            id=query.upper(),
-            title='Convert to UPPER TEXT',
-            input_message_content=InputTextMessageContent(query.upper())
-        )
-    )
-    context.bot.answer_inline_query(update.inline_query.id, results)
+    # Создаем экземпляр бота и получаем его токен
+    bot = telegram.Bot(token='YOUR_BOT_TOKEN')
 
-# функция обработки не распознных команд
-def unknown(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id,
-                             text="Sorry, I didn't understand that command.")
+    # Создаем экземпляр Updater и передаем ему бота
+    updater = Updater(bot=bot, use_context=True)
 
-# обработчик команды '/start'
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+    # Добавляем обработчики команд и текстовых сообщений
+    updater.dispatcher.add_handler(CommandHandler('start', start))
+    updater.dispatcher.add_handler(CommandHandler('help', help))
+    updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
 
-# обработчик текстовых сообщений
-echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-dispatcher.add_handler(echo_handler)
+    # Запускаем бота
+    updater.start_polling()
 
-# обработчик команды '/caps'
-caps_handler = CommandHandler('caps', caps)
-dispatcher.add_handler(caps_handler)
+    # Запускаем цикл обработки сообщений
+    updater.idle()
 
-# обработчик встроенных запросов
-inline_caps_handler = InlineQueryHandler(inline_caps)
-dispatcher.add_handler(inline_caps_handler)
-
-# обработчик не распознанных команд
-unknown_handler = MessageHandler(Filters.command, unknown)
-dispatcher.add_handler(unknown_handler)
-
-# запуск прослушивания сообщений
-updater.start_polling()
-# обработчик нажатия Ctrl+C
-updater.idle()
+if __name__ == '__main__':
+    main()
